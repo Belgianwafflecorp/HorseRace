f3030f7755d3b1b641106fbf0c82e391a5a100c7
belgianwafflecorp <domien.verstraete@student.vives.be>
2024-06-13 18:47:00 +0200

    chore: Add horse class and settings module

---
+++ horse.py
@@ -0,0 +1,21 @@
+import random
+from settings import settings
+
+class horse:
+    def __init__(self, name, breed):
+        self.name = name
+        self.horse_stats = settings.horse_stats
+        self.settings = settings()
+        
+    def new_horse(self):
+        self.name = random.choice(self.settings.horse_names)
+        self.settings.horse_names.pop(self.name)
+        self.horse_stats = self.stats()
+
+    def stats(self):
+        for stat in self.horse_stats:
+            self.horse_stats[stat] = random.randint(1, 100)
+        return self.horse_stats
+    
+    def horse_avg(self):
+        return sum(self.horse_stats.values()) / len(self.horse_stats)
---
+++ settings.py
@@ -0,0 +1,45 @@
+import random
+
+class settings:
+    def __init__(self):
+        ...
+        
+    horse_names = [
+        'Amber', 
+        'Angel', 
+        'Autumn', 
+        'Bella', 
+        'Blaze', 
+        'Chance', 
+        'Comet', 
+        'Daisy', 
+        'Delta', 
+        'Favory', 
+        'Hidalgo', 
+        'Lightning', 
+        'Lucky', 
+        'Max', 
+        'Midnight', 
+        'Misty', 
+        'Scout', 
+        'Shadow', 
+        'Spirit',
+        'Star', 
+        'Storm', 
+        'Sunny', 
+        'Thunder', 
+        'Willow'
+    ]
+
+    horse_stats = {
+        'speed': 0,
+        'strength': 0,
+        'weight': 0,
+        'stamina': 0,
+        'training': 0,
+    }
+
+    
+
+
+
