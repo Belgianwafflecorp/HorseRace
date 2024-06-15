c1a996f4c77860880b2a576086d2dc58e68bbb64
belgianwafflecorp <domien.verstraete@student.vives.be>
2024-06-15 09:30:47 +0200

    chore: Refactor horse class and race module

--- horse.py
+++ horse.py
@@ -6,6 +6,9 @@ class horse:
         self.name = name
         self.horse_stats = settings.horse_stats
         self.settings = settings()
+        self.horse_stats = self.stats()
+        self.horse_avg = self.horse_avg()
+        self.position = ['H','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_','_']
         
     def new_horse(self):
         self.name = random.choice(self.settings.horse_names)
@@ -18,6 +21,31 @@ class horse:
         return self.horse_stats
     
     def horse_avg(self):
-        return sum(self.horse_stats.values()) / len(self.horse_stats)
+        return int(sum(self.horse_stats.values()) / len(self.horse_stats))
     
-    
+    def horse_run_odds(self):
+        for horse in self.race_horses:
+            move_odds = horse.horse_avg()
+            no_move_odds = 100 - move_odds
+        
+            self.run_odds = {
+                'move': move_odds,
+                'no_move': no_move_odds
+            }
+
+            # Randomly choose between move and no_move based on the odds
+            self.move_result = random.choices(['move', 'no_move'], weights=[move_odds, no_move_odds], k=1)[0]
+            
+
+    def horse_position(self):
+        if self.move == 'move':
+            self.position.pop(-1)
+            self.position.append('_')
+            print(self.position)
+        print(self.position)
+
+    def horse_run(self):
+        for horse in self.race_horses:
+            self.move = random.choices(list(self.run_odds.keys()), k = 1, weights = self.run_odds.values)
+
+            self.horse_position()
--- race.py
+++ race.py
@@ -19,27 +19,4 @@ class race:
 
 
 
-    def horse_run_odds(self):
-        for horse in self.race_horses:
-            move_odds = horse.horse_avg()
-            no_move_odds = 100 - move_odds
-        
-            run_odds = {
-                'move': move_odds,
-                'no_move': no_move_odds
-            }
-
-    def horse_position(self):
-        s = '_'
-        position = ['H',s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s]
-        if self.move == 'move':
-            position.pop(-1)
-            position.append(s)
-            print(position)
-        print(position)
-
-    def horse_run(self):
-        for horse in self.race_horses:
-            self.move = random.choices(list(self.run_odds.keys()), k = 1, weights = self.run_odds.values)
-
-            self.horse_position()
+    
