4d278aee67b87ce9f901fdcd68a8554bf4538fc1
belgianwafflecorp <domien.verstraete@student.vives.be>
2024-06-16 21:01:45 +0200

    windows terminal install

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from race import Race
+import os
+
+# winget install --id Microsoft.WindowsTerminal -e
+# Windows Terminal is not required to run the game, but it is recommended for a better experience.
+os.system("winget install --id Microsoft.WindowsTerminal -e")
 
 def main():
     while True:
